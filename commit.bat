86548207f1a8ea7ef79a7eae7fbe3c8a53e8b57a
kirin123kirin <kirin123kirin@gmail.com>
2018-03-03 22:53:57 +0900

    clean

--- commit.bat
+++ commit.bat
@@ -4,4 +4,5 @@ cd %~dp0
 git pull
 git add .
 git commit -a
-git push -u orgin master
+git push -u origin master
+pause
--- jupyterlab.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-%~d0
-cd %~dp0
-cd ..\Documents\dev\spyder_dev
-
-jupyter lab
-
