f1944015e92a2579a47dda4c4c9609b9c5b8792d
kirin123kirin <kirin123kirin@gmail.com>
2018-08-31 23:57:00 +0900

    ok

--- awsdownload.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd %~dp0
-
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" aws /console /script="%APPROOT%\usr\local\bin\awsdownload.wsp"
-
-start himodownload.bat
-
--- awsupload.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %~dp0
-
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" aws /console /script="%APPROOT%\cmd\awsupload.wsp"
-
-call himoupload.bat
