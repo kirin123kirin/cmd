38fbb8bf082503d5862b0ae6d4716f3a6dc82547
kirin123kirin <kirin123kirin@gmail.com>
2019-06-09 10:58:14 +0900

    lslog class taste modify

--- lslog.py
+++ lslog.py
@@ -1,115 +1,183 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pandas._libs.tslibs.parsing import parse_time_string
 import os, re, sys
+from glob import  glob
 from datetime import datetime
 from io import StringIO
-import codecs
 from pathlib import Path
+from dateutil.parser._parser import parser, parserinfo
 
-def parse_date(s, base_date = "", future_date=True,
-        mod_d = re.compile("\s*[年月日]\s*"),
-        mod_t = re.compile("\s*[時分秒]\s*"),
-        callback=None):
-    
-    s = mod_d.sub("/", s)
-    s = mod_t.sub(":", s)
-    
-    dt =parse_time_string(s)[0]
-    
-    if dt.year == 1:
-        
-        if base_date:
-            b = mod_d.sub("/", base_date)
-            b = mod_t.sub(":", b)
-            base_date = parse_time_string(b)[0]
-        else:
+class _jpinfo(parserinfo):
+    WEEKDAYS = [
+            ("Mon", "/曜日", "/曜", "Monday"),
+            ("Tue", "火曜日", "火曜", "火", "Tuesday"),
+            ("Wed", "水曜日", "水曜", "水", "Wednesday"),
+            ("Thu", "木曜日", "木曜", "木", "Thursday"),
+            ("Fri", "金曜日", "金曜", "金", "Friday"),
+            ("Sat", "土曜日", "土曜", "土", "Saturday"),
+            ("Sun", "日曜日", "日曜", "日", "Sunday")]
+    HMS = [
+            ("h", "時", "hour", "hours"),
+            ("m", "分", "minute", "minutes"),
+            ("s", "秒", "second", "seconds")]
+    AMPM = [
+            ("am", "ａｍ", "午前", "a"),
+            ("pm", "ｐｍ", "午後", "p")]
+
+def to_datetime(timestr, parserinfo=_jpinfo(), **kwargs):
+    timestr = timestr.replace("年", "/").replace("月", "/")
+    return parser(info=parserinfo).parse(timestr, **kwargs)
+
+def parse_date(s,
+        base_date = datetime.now(),
+        future_date = True,
+        callback = None):
+
+    dt = to_datetime(s)
+
+    if future_date is False:
+        if not base_date:
             base_date = datetime.now()
-        
-        check = dt.replace(year=base_date.year)
-        
-        if future_date is False and check > base_date:
+        elif isinstance(base_date, str):
+            base_date = to_datetime(base_date)
+
+        if dt > base_date:
             dt = dt.replace(year=base_date.year - 1)
-        else:
-            dt = check
-    
+
     return callback(dt) if callback else dt
 
-def split_date_time(dt):
+def _split_date_time(dt):
     return dt.strftime("%Y/%m/%d"), dt.strftime("%H:%M:%S")
 
-def parse_ls(line, parent="", base_date="", mod_sp = re.compile(" +")):
-    if line.startswith("l"):
-        p, _, o, g, size, *d, name, _, ln = mod_sp.split(line)
-    else:
-        ln = ""
-        p, _, o, g, size, *d, name = mod_sp.split(line)
-    
-    dt = parse_date(" ".join(d), base_date, future_date=False, callback=split_date_time)
-    
-    ret = [p, o, g, size, *dt, name.split(".")[-1] if "." in name else "", name]
-    if not parent:
-        return [*ret, ln]
-    fullpath = parent.rstrip("/") + "/" + name
-    dirname = fullpath if p.startswith("d") else parent.rstrip("/")
-    return [*ret, fullpath, ln, *map("/".__add__, dirname.split("/"))]
-    
-def opener(f, *args, **kw):
-    if hasattr(f, "read"):
-        return f
-    if isinstance(f, (str, Path)) and os.path.exists(f):
-        return codecs.open(f, *args, **kw)
-    return StringIO(f)
 
-def lslog(lines, parent = "", base_date=""):
-    exist_p = re.compile("^[^\s].*:$").match
-    exist_c = re.compile(r"^[drwxtslc\-]{10} .*").match
-    
-    current = parent
-    for line in lines:
-        if not line:
-            continue
-        
-        line = line.rstrip()
-        
-        if exist_c(line):
-            yield parse_ls(line, current, base_date)
-        elif exist_p(line):
-            if parent:
-                current = parent + line.strip(" :").lstrip(".")
+class lstab(object):
+    def __init__(self, file, parent = "", base_date=""):
+        if isinstance(file, (list, tuple)):
+            self.file = sum(map(glob, file), [])
+        else:
+            self.file = glob(file)
+        self.parent = parent
+        self.base_date = base_date
+        self.header = ["permission", "owner", "group", "size", "date", "time",
+                       "extention", "basename", "link_path", "fullpath"]
+
+        self.fp = (line for f in self.file for line in self.open(f))
+        self.rundate = re.compile("^(?:\[[\d\-/:\. ]{8,36}\] )")
+        self.exist_p = re.compile(self.rundate.pattern + "?[^\s].*:$").match
+        self.exist_c = re.compile(self.rundate.pattern + "?[drwxtslc\-]{10} .*").match
+        self.mod_sp = re.compile(" +")
+        self.df = None
+        self.ncol = 0
+
+    def parse_ls(self, line, parent=""):
+        if line.startswith("l"):
+            p, _, o, g, size, *d, name, _, ln = self.mod_sp.split(line)
+        else:
+            ln = ""
+            p, _, o, g, size, *d, name = self.mod_sp.split(line)
+
+        dt = parse_date(" ".join(d), self.base_date, future_date=False, callback=_split_date_time)
+        try:
+            size = int(size)
+        except:
+            pass
+        ret = [p, o, g, size, *dt, name.split(".")[-1] if "." in name else "", name]
+        parent = parent or self.parent
+        if not parent:
+            return [*ret, ln]
+        fullpath = parent.rstrip("/") + "/" + name
+        self.ncol = max(fullpath.count("/"), self.ncol)
+        dirname = (fullpath if p.startswith("d") else parent).strip("/")
+        return [*ret, ln, fullpath, *map("/".__add__, dirname.split("/"))]
+
+    def open(self, file, *args, **kw):
+        if hasattr(file, "read"):
+            return file
+        if isinstance(file, (str, Path)) and os.path.exists(file):
+            try:
+                fp = open(file, encoding="utf-8", *args, **kw)
+                fp.read(1)
+            except UnicodeDecodeError:
+                fp = open(file, encoding="cp932", *args, **kw)
+                fp.read(1)
+            fp.seek(0)
+            return fp
+        return StringIO(file)
+
+    def __iter__(self):
+        current = self.parent
+
+        for line in self.fp:
+            line = self.rundate.sub("", line)
+            if not line:
+                continue
+
+            line = line.rstrip()
+
+            if self.exist_c(line):
+                yield self.parse_ls(line, current)
+            elif self.exist_p(line):
+                if self.parent:
+                    current = self.parent + line.strip(" :").lstrip(".")
+                else:
+                    current = line.strip(" :")
+
+    def to_csv(self, filepath_or_buffer, sep=',', index=False, quotechar='"', quoting=0, **kw):
+        import csv
+        fp = filepath_or_buffer if hasattr(filepath_or_buffer, "write") else open(filepath_or_buffer, "w")
+        if fp == sys.stdout:
+            kw["lineterminator"] = "\n"
+            writer = csv.writer(fp, delimiter=sep, quotechar=quotechar, quoting=quoting, **kw)
+            if self.header:
+                writer.writerow(self.header + ["DIRS*"])
+            for x in self.__iter__():
+                writer.writerow(x)
+                fp.flush()
+        else:
+            return self.to_pandas().to_csv(filepath_or_buffer, sep=sep, index=index, quotechar=quotechar, quoting=quoting, **kw)
+
+    def to_excel(self, outfile):
+        from xlsxwriter import Workbook
+
+        with Workbook(outfile) as book:
+            header_fmt = book.add_format(
+                dict(border=True, align="center", bold=True))
+
+            sheet = book.add_worksheet()
+
+            for i, row in enumerate(self.__iter__(), 1):
+                sheet.write_row(i, 0, row)
+
+            if not sheet.dim_colmax:
+                sys.stderr.write("No Data\n")
+                return
+
+            if self.header:
+                dirs = map("DIR{:02d}".format, range(sheet.dim_colmax - 9))
+                sheet.write_row(0, 0, [*self.header, *dirs], header_fmt)
+                sheet.autofilter(0, 0, sheet.dim_rowmax, sheet.dim_colmax)
+
+    def to_pandas(self):
+        from pandas import DataFrame
+        if self.df is None:
+            self.df = DataFrame(self.__iter__())
+            if len(self.df.columns) == 9:
+                self.df.columns = self.header[:-1]
             else:
-                current = line.strip(" :")
+                self.df.columns = [*self.header, *map("DIR{:02d}".format, range(self.df.shape[1] - len(self.header)))]
+        return self.df
 
-HEADER = ["permission", "owner", "group", "size", "date", "time", "extention", "basename", "fullpath", "link_path"]
+    def to_html(self, buf=None, columns=None, col_space=None, header=True, index=False, **kw):
+        return self.to_pandas().to_html(buf, columns, col_space, header, index, **kw)
 
-def to_csv(iterator, file=sys.stdout, header=HEADER, **kw):
-    import csv
-    fp = file if hasattr(file, "write") else open(file, "w")
-    if fp == sys.stdout:
-        kw["lineterminator"] = "\n"
-    writer = csv.writer(fp, **kw)
-    if header:
-        writer.writerow(header + ["DIRS*"])
-    for x in iterator:
-        writer.writerow(x)
-        fp.flush()
-    if not hasattr(file, "write"):
-        fp.close()
-
-def to_excel(iterator, file, header=HEADER):
-    from xlsxwriter import Workbook
-    with Workbook(file) as book:
-        sheet = book.add_worksheet()
-        for i, row in enumerate(iterator, 1):
-            sheet.write_row(i, 0, row)
-        if header:
-            sheet.write_row(0, 0, [*header, *range(sheet.dim_colmax - 9)])
+    def to_json(self, path_or_buf=None, orient="records", **kw):
+        return self.to_pandas().to_json(path_or_buf, orient=orient, **kw)
 
 def main():
     from argparse import ArgumentParser
     from glob import glob
-    
+
     usage="""
     parse from `ls -lR` log string
        Example1: {0} *.log -o fileslist.csv
@@ -119,7 +187,7 @@ def main():
 
     ps = ArgumentParser(usage)
     padd = ps.add_argument
-    
+
     padd('-o', '--outfile', type=str, default=None,
          help='output filepath (default `stdout`)')
     padd('-b', '--basedate', type=str, default=None,
@@ -131,24 +199,29 @@ def main():
          nargs="+",  default=[],
          help="target mlocate.db files")
     args = ps.parse_args()
-    
+
     outfile = args.outfile or sys.stdout
     BASE_DATE = args.basedate
     PARENT = args.currentdirectory
-    
+
     files = [g for fn in args.filename for g in glob(fn)]
     if not files:
         raise RuntimeError("Not found files {}".format(args.filename))
-    
-    it = (x for file in files for x in lslog(opener(file), PARENT, BASE_DATE))
-    
-    if str(outfile).lower().rsplit(".", 1)[1].startswith("xls"):
-        to_excel(it, outfile)
+
+    tb = lstab(files, parent=PARENT, base_date=BASE_DATE)
+    ext = str(outfile).lower().rsplit(".", 1)[1]
+    if ext.startswith("xls"):
+        tb.to_excel(outfile)
+    elif ext.startswith("htm"):
+        tb.to_html(outfile)
+    elif ext.startswith("json"):
+        tb.to_json(outfile)
     else:
-        to_csv(it, outfile)
-    
+        tb.to_csv(outfile)
+
 
 if __name__ == "__main__":
-    #sys.argv.extend('lsdir.log -c /hoge/foo -o test.xlsx'.split(" "))
-    
+#    sys.argv.extend(r"C:/temp/lsdir.log -o C:/temp/test.xlsx".split(" "))
+    sys.argv.append(r"C:/temp/lsdir.log")
+
     main()
