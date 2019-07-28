393802156141ffcd0934f330141f77b9146ec7aa
kirin123kirin <kirin123kirin@gmail.com>
2019-07-29 05:56:41 +0900

    profiler remake

--- util/profiler.py
+++ util/profiler.py
@@ -8,176 +8,217 @@ require pandas and dask!!
 MIT License
 
 """
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "m.yama"
 
 
 __all__ = [
         "profiler",
-        "Profile",
-        "profiler_df",
+        "profile_data",
 ]
 
-
-from util.dfutil import read_any
-from util.core import isdataframe
-
+from itertools import  zip_longest
+from  collections import  _count_elements
+import heapq
+from collections import namedtuple
+import sys
+import csv
+import os
 from itertools import combinations
 
-import sys
-import os
-from io import StringIO
+from util import Path
 
-# 3rd party modules
-import pandas as pd
-import dask.dataframe as dd
-import numpy as np
+ret = namedtuple("Profile",
+    ["rec_count", "is_notnull", "is_uniq", "notnull_count", "null_count", "uniq_count", "fill_rate", "uniq_rate", "key_rate", "top"]
+)
 
 
-def skew(key):
-    if(len(key)) > 20: # for memory over flow
-        key = dict(sorted(key.items(), key=lambda x: x[1], reverse=True)[:20])
-    for i in range(1, len(key)):
-        for x in combinations(key, i):
-            yield sum(key[k] for k in x), x
+def guess_key(profile, n=10):
+    keys = sorted([(v.key_rate, k) for k, v in profile.items()])[-19:]
+    result = []
+    for i in range(1, len(keys)):
+        for x in combinations(keys, i):
+            rk = []
+            rv = 0
+            for v, k in x:
+                rk.append(k)
+                rv += v ** v
+            result.append((rv**0.5, rk))
+    return [k for v, k in sorted(result, reverse=True)[:n]]
 
-def _co_profiler(f, df, top):
-    for k, cdf in (df.items() if isinstance(df, dict) else df):
-        cdf = Profile(cdf, top=top).data
-        cdf.insert(0, "sheetname_or_table", k)
-        cdf.insert(0, "filename", f)
-        yield k, cdf
+def profile_data(rows, header=None, top=10, na_val=[None, "", "N/A", "NULL", "null", "none", "na"]):
+    result = {}
+    
+    rec = None
+    col = []
+    
+    if isinstance(header, int):
+        if hasattr(rows, "__next__"):
+            for i in range(header + 1):
+                col = next(rows)
+        else:
+            col, *rows = rows[header:]
+    elif isinstance(header, (list, tuple)):
+        col = header
+    
+    for i, x in enumerate(zip_longest(*rows)):
+        if rec is None:
+            rec = len(x)
+        
+        counter = {}
+        gc = counter.get
+        
+        _count_elements(counter, [y for y in x if y not in na_val])
+        
+        uq_n = len(counter)
+        uq_rate = uq_n / rec 
+        
+        notna_n = sum(counter.values())
+        
+        topN = heapq.nlargest(top, counter, key=gc) if top > 0 else []
+        
+        del counter, gc
+        
+        na_n = rec - notna_n
+        fill_rate = notna_n / rec
+        if uq_rate > 0 and fill_rate > 0:
+            key_rate = (uq_rate * fill_rate) / ((pow(uq_rate, 2) + pow(fill_rate, 2)) ** 0.5)
+        else:
+            key_rate = 0.0
+        
+        info = ret(
+            rec,
+            rec == notna_n,
+            rec == uq_n,
+            notna_n,
+            na_n,
+            uq_n,
+            fill_rate,
+            uq_rate,
+            key_rate,
+            topN
+        )
+        
+        k = col[i] if i < len(col) else i
+        result[k] = info
+    
+    return result
 
-def profiler(f, top=10, header=True, outputencoding="cp932", *args, **kw):
-    df = read_any(f, *args, **kw)
-    sio = StringIO()
+def profiler(
+    path_or_buffer,
+    header=None,
+    top=10,
+    na_val = [None, "", "N/A", "NULL", "null","none", "na"],
+    headerout=True,
+    ):
 
-    if isdataframe(df):
-        df = [["-", df]]
+    kw = dict(
+        header=header,
+        top=top,
+        na_val=na_val,
+    )
+    
+    p = Path(path_or_buffer)
+    
+    head = []
+    
+    try:
+        rows = p.groupbylines()
+        
+        if headerout:
+            head = [["targetname", "columns", *ret._fields]]
+        return head + [[pk, ck, *cv] for _, pk, row in rows for ck, cv in profile_data(row, **kw).items()]
+    except ValueError:
+        rows = p.iterlines()
+        if headerout:
+            head = [["columns", *ret._fields]]
+        return head + [[k, *v] for k, v in profile_data(rows, **kw).items()]
 
-    i = 0
-    for k, cdf in _co_profiler(f, df, top):
-        cdf.to_csv(sio, index=False, sep=",", header=i == 0 and header, encoding=outputencoding)
-        i += 1
-    sio.seek(0)
-    return sio.getvalue()
 
-def profiler_df(f, top=10, *args, **kw):
-    df = read_any(f, *args, **kw)
+def to_excel(rows, outputfile, sheetname="Sheet1",
+            header = True, startrow=0, startcol=0, conditional_value=" ---> "):
+    import xlsxwriter
 
-    if isdataframe(df):
-        df = [["-", df]]
+    i = startrow
+    j = startcol
 
-    return pd.concat([cdf for k, cdf in _co_profiler(f, df, top)])
+    with xlsxwriter.Workbook(outputfile) as wb:
+        ws = wb.add_worksheet(sheetname)
+        write = ws.write_row
 
-class Profile(object):
-    dnrule = "diff{:02d}".format
-
-    def __init__(self, df, top=10, percentile=95):
-        self.df = df if (df.dtypes == object).all() else df.astype("str")
-        self._top = top
-        self.top = top
-        self._profiler = None
-        self._guesskey = None
-        self._diffkey = None
-        self._percentile = percentile
-        self.percentile = percentile
-
-    @property
-    def data(self):
-        if self._profiler is None:
-            cols = self.df.columns.tolist()
-            rec, ncol = len(self.df), len(cols)
-
-            if ncol + rec == 0:
-                return pd.DataFrame(columns=['cols','rec','count','unique','uniqrate','valrate','keyrate','top'])
-
-            if hasattr(self.df, "compute"):
-                cnt = self.df.mask(self.df == "", np.nan).count().compute()
-                uniq = self.df.nunique().compute()
-                topfx = lambda c: self.df[c].value_counts().head(min(self.top or uniq[c], uniq[c])).index.tolist()
+        if header:
+            if hasattr(rows, "__next__"):
+                row = next(rows)
             else:
-                cnt = self.df.replace("", np.nan).count()
-                uniq = self.df.nunique()
-                if self.top:
-                    topfx = lambda c: self.df[c].value_counts().head(self.top).index.tolist()
-                else:
-                    topfx = lambda c: self.df[c].value_counts().index.tolist()
+                row, *rows = rows
+            props_h = dict(border=1, bold=True, align="center", valign="center")
+            write(i, startcol, row, wb.add_format(props_h))
+            i += 1
 
-            self._profiler = pd.DataFrame(dict(
-                        cols=cols,
-                        rec=[rec] * ncol,
-                        count=cnt,
-                        unique=uniq,
-                        uniqrate = uniq / rec,
-                        valrate = cnt / rec,
-                        keyrate = uniq * cnt / rec ** 2,
-                        top = [topfx(c) for c in cols]
-                        ))
-            self._profiler.reset_index(drop=True, inplace=True)
-        return self._profiler
+        border = wb.add_format(dict(border=1, valign="center"))
 
-    @property
-    def guesskey(self):
-        if self._guesskey is None or self._top != self.top:
-            dt = self.data.loc[self.data.keyrate > 0, ["cols", "keyrate"]]
-            self._guesskey = dt.set_index("cols").keyrate.to_dict()
-        return self._guesskey
+        for row in rows:
+            write(i, startcol, row, border)
+            icol = startcol + len(row) - 1
+            if j < icol:
+                j = icol
+            i += 1
 
-    @property
-    def diffkey(self):
-        if self._diffkey is None or self._percentile != self.percentile:
-            df = pd.DataFrame(skew(self.guesskey), columns=["skw", "key"])
-            pt = df.skw.quantile(self.percentile / 100.0)
-            df = df[(df.skw > pt) | (df.key.apply(len) == len(self.guesskey)-1)].sort_values("skw", ascending=False)
-            df.index = list(map(self.dnrule, range(1, df.index.size + 1)))
+        ws.autofilter(startrow, startcol, i, j)
 
-            self._diffkey = df.key.to_dict()
-        return self._diffkey
+        redfm = wb.add_format(
+            dict(bg_color='#FFC7CE',
+                font_color='#9C0006')
+        )
 
-    def __getitem__(self, name):
-        k = self.dnrule(int(name.split("diff")[-1]))
-        return self.diffkey[k]
+        ws.conditional_format(startrow, startcol, i, j,
+            dict(type='text',
+                criteria='containing',
+                value=conditional_value,
+                format=redfm)
+        )
 
-    def __getattr__(self, name):
-        return self.__getitem__(name)
+defaultencoding = "cp932" if os.name == "nt" else "utf-8"
 
-
+def to_csv(rows, outputfile, encoding, lineterminator="\r\n", **kw):
+    with open(outputfile, "w", encoding=encoding, newline="") as f:
+        writer = csv.writer(f, lineterminator=lineterminator, **kw)
+        writer.writerows(rows)
+        
+def to_tsv(rows, outputfile, encoding, lineterminator="\r\n", **kw):
+    return to_csv(rows, outputfile, encoding, lineterminator, delimiter="\t", quoting=csv.QUOTE_NONE, **kw)
+    
+    
 def main():
-    from argparse import ArgumentParser
     from glob import glob
-    import codecs
+    from argparse import ArgumentParser
 
-    usage="""
-    Any file Data Profiler
-       Example1: {0} [-v] [-n 30] [--encoding utf-8] "C:\\hoge\\test.csv"
-       Example2: {0} *.xlsx
-
-    """.format(os.path.basename(sys.argv[0]).replace(".py", ""))
-
-    ps = ArgumentParser(usage)
+    ps = ArgumentParser(prog="differ",
+                        description="2 file diff compare program\n")
     padd = ps.add_argument
 
-    padd("-v", "--verbose",
-         help="処理中のファイル名を表示する",
+    padd('-V','--version', action='version', version='%(prog)s ' + __version__)
+    padd("-v", "--verbose", help="print progress",
          action='store_true', default=False)
-    padd("-n", "--numtop",
-         help="TOPn件のサンプルデータを表示する(デフォルトtop10)",
-         type=int, default=10)
-    padd("--encoding",
-         help="input file encoding default auto",
-         default=None)
-    padd("--outputencoding",
-         help="出力ファイルのエンコーディングを指定する(デフォルトはcp932)\n選択肢cp932 utf-8 eucjp",
-         default="cp932")
-    padd('-o', '--outfile', type=str, default=None,
-         help='output filepath (default `stdout`)')
     padd("filename",
          metavar="<filename>",
          nargs="+",  default=[],
-         help="target any files(.txt, .csv, .tsv, .xls[x], .accdb, .sqlite3)")
+         help="target any files(.txt, .csv, .tsv, .xls[x],)")
+    
+    padd('-o', '--outfile', type=str, default=None,
+         help='output filepath (default `stdout`)')
+    padd('-e', '--encoding', type=str, default="cp932",
+         help='output fileencoding (default `cp932`)')
+    padd('-l', '--lineterminator', type=str, default="\r\n",
+         help='output fileencoding (default `\\r\\n`)')
+    padd('-H', '--header', type=int, default=0,
+         help='number of header Input File index. (start is 0) (default 0)')
+    padd('-N', '--navalue', type=str, default=[],
+         help='output index N/A value (default `-`)')
+    padd('-t', '--top', type=int, default=10,
+         help='frequency values top count (default `10`)')
     args = ps.parse_args()
-
+    
     def walk(args):
         for arg in args.filename:
             for f in glob(arg):
@@ -187,82 +228,128 @@ def main():
                     sys.stderr.flush()
                 yield f
 
-    if args.outfile and os.path.splitext(args.outfile)[1].lower().startswith(".xls"):
-        from openpyxl.utils import get_column_letter
-        from openpyxl.formatting.rule import ColorScaleRule
+    na_value = [None, "", "N/A", "NULL", "null","none", "na"]
+    na_value.extend(args.navalue.split(",") if args.navalue else args.navalue)
+    header = args.header
+    encoding = args.encoding
+    lineterminator = args.lineterminator
+    top = args.top
+    
+    def it():
+        i = None
+        for i, f in enumerate(walk(args)):
+            j = 0
+            for x in profiler(
+                f,
+                header=header,
+                top=top,
+                na_val=na_value,
+                headerout= i == 0,
+                ):
+                if i + j == 0:
+                    yield ["filename"] + x
+                else:
+                    yield [f] + x
+                j += 1
+        if i is None:
+            raise FileNotFoundError(str(args.filename))
+    
+    if args.outfile:
+        outputfile = Path(args.outfile)
+    else:
+        writer = csv.writer(sys.stdout, lineterminator="\n")
+        return writer.writerows(it())
+    
+    ext = outputfile.ext.startswith
+    if ext(".xls"):
+        to_excel(it(), outputfile, header=header, conditional_value="False")
+    elif ext(".tsv"):
+        to_tsv(it(), outputfile, encoding=encoding, lineterminator=lineterminator)
+    else:
+        to_csv(it(), outputfile, encoding=encoding, lineterminator=lineterminator)
 
-        ret = pd.concat([profiler_df(f, top=args.numtop) for f in walk(args)])
-        with pd.ExcelWriter(args.outfile, engine="openpyxl") as writer:
-            ret.to_excel_plus(writer, index=True,
-                title="Profile Data for {}".format(args.filename))
-            sheet = writer.sheets["Sheet1"]
-            cols = ret.columns.tolist()
-            en = sheet.max_row
-            rate_cols = [get_column_letter(cols.index(x) + 2) for x in ret if "rate" in x]
-            for rc in rate_cols:
-                target = '{}2:{}{}'.format(rc, rc, en)
-                sheet.conditional_formatting.add(
-                    target,
-                    ColorScaleRule(
-                        start_type='min', start_value=0.0, start_color="FFFFFF",
-                        end_type='max', end_value=1.0, end_color="FFA500"))
-                sheet.column_dimensions[rc].width = 10
-                for (c,) in sheet[target]:
-                    c.number_format = '0.00%'
-            writer.save()
-        return
-
-    out = codecs.open(args.outfile, mode="w", encoding=args.outputencoding) if args.outfile else sys.stdout
-    i = 0
-    for f in walk(args):
-        print(profiler(f, args.numtop, i == 0, outputencoding=args.outputencoding), file=out)
-        i += 1
-
-    if i == 0:
-        raise ValueError("ファイルが見つかりませんでした\n\n" + usage)
-
-"""
-   TestCase below
-"""
 
 def test():
-    from util.core import tdir
     from datetime import datetime as dt
-
-    def test_Profiler():
-        f = tdir + "diff1.csv"
-        df = read_any(f).head(3)
-        pr = Profile(df)
-        print(pr.data.to_csv(index=False))
-
+    from util.core import tdir
+    from io import StringIO
+    
+    def test_profile_data():
+        a = [list("ABCD")] + [list("abcd")] * 3
+        r = profile_data(a)[0]
+        assert(r.rec_count == 4)
+        assert(r.is_notnull is True)
+        assert(r.is_uniq is False)
+        assert(r.top == list("aA"))
+    
+    def test_profile_data_uniq():
+        a = zip(*[list(range(10))])
+        r = profile_data(a)[0]
+        assert(r.rec_count == 10)
+        assert(r.is_notnull is True)
+        assert(r.is_uniq is True)
+        assert(r.top == list(range(10)))
+        
+    def test_profile_data_header_0():
+        a = [list("ABCD")] + [list("abcd")] * 3
+        r = profile_data(a, header=0)["A"]
+        assert(r.rec_count == 3)
+        assert(r.is_notnull is True)
+        assert(r.is_uniq is False)
+        assert(r.top == list("a"))
+    
+    def test_profile_data_header_n():
+        a = [list("ABCD")] + [list("abcd")] * 3
+        r = profile_data(a, header=2)["a"]
+        assert(r.rec_count == 1)
+        assert(r.is_notnull is True)
+        assert(r.is_uniq is True)
+        assert(r.top == list("a"))
+        
+    def test_profile_data_topn():
+        a = [list("ABCD")] + [list("abcd")] * 3
+        r = profile_data(a, top=1)[0]
+        assert(len(r.top) == 1)
+        r = profile_data(a, top=0)[0]
+        assert(r.top == [])
+    
+    def test_profile_data_naval():
+        a = [list("AB")] + [[None] * 2, [""] * 2, ["N/A", "hoge"]]
+        rr = profile_data(a, header=0)
+        r = rr["A"]
+        assert(r.rec_count == 3)
+        assert(r.null_count == 3)
+        assert(rr["B"].null_count == 2)
+        assert(rr["B"].notnull_count == 1)
+    
+    def test_guess_key():
+        a = profile_data(Path(tdir+"diff1.xlsx").readlines(), header=0)
+        assert(len(guess_key(a, 10)) == 10)
+    
     def test_profiler():
-        f = tdir + "diff1.csv"
-        print(profiler(f, top=None))
-
-
-    def test_diffkey_pandas():
-        f = tdir + "diff1.csv"
-        df = read_any(f).head(3)
-        t1 = dt.now()
-        pr = Profile(df)
-        pr.diffkey
-        t2 = dt.now()
-        print("diffkey_pandas", t2-t1)
-
-    def test_diffkey_dask():
-        f = tdir + "diff1.csv"
-        df = dd.from_pandas(read_any(f).head(3),1)
-        t1 = dt.now()
-        pr = Profile(df)
-        pr.guesskey
-        pr.diffkey
-        t2 = dt.now()
-        print("diffkey_dask", t2-t1)
-
-    def test_main():
-        sys.argv.extend(["-v", tdir + "sample.accdb"])
+        r = profiler(tdir+"test.csv")
+        assert(len(r) == 3)
+        assert(r[0][0] == "columns")
+        assert(len(r[0]) == len(r[1]))
+        assert(r[1][1] == 3)
+    
+    def test_benchmark():
+        profiler(tdir+"diff1.csv")
+    
+    def stdoutcapture(args):
+        sio = StringIO()
+        sys.stdout = sio
+        del sys.argv[1:]
+        sys.argv.extend(args)
         main()
-
+        sio.seek(0)
+        sys.stdout = sys.__stdout__
+        return sio
+        
+    def test_main():
+        sio = stdoutcapture([tdir+"diff1.xlsx"])
+        assert(sio.getvalue().count("\n") == 10)
+    
     for x, func in list(locals().items()):
         if x.startswith("test_") and callable(func):
             t1 = dt.now()
@@ -270,7 +357,6 @@ def test():
             t2 = dt.now()
             print("{} : time {}".format(x, t2-t1))
 
-
 if __name__ == "__main__":
-#    test()
+    #test()
     main()
